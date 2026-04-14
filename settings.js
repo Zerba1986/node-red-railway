2f141410d1b49d1bbab05569bb5d99c44b98c9f2
Zerba1986 <joazeirofelipe@gmail.com>
2026-04-14 08:44:00 -0300

    settings.js

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2y$08$t/rgP1k5tQvpH7zxEdqRc.pqn/7.Yhs3shMEQ/y6V8EyNYwgRFmOm",
+            permissions: "*"
+        }]
+    }
+}
